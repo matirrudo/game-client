841c6d3f5cc1f57dabcf81ea5ea06d0c127c2337
matias <matirrudo@gmail.com>
2021-11-23 17:06:59 -0300

    Video de presentacion

--- src/scenes/pausa.js
+++ src/scenes/pausa.js
@@ -4,19 +4,17 @@ import { LevelsButton } from "../components/levels-button.js";
 export class Pausa extends Phaser.Scene {
   constructor() {
     super({ key: 'Pausa' });
-    this.startButton = new StartButton(this);
-    this.levelsButton = new LevelsButton(this);
   }
 
   preload() {
     this.load.image('menu', '../../assets/images/backgrounds/MenuPausa.png');
-    this.startButton.preload();
-    this.levelsButton.preload();
   }
   
   create() {
     this.congratsImage = this.add.image(0, 600, 'menu').setOrigin(0,1);
-    this.startButton.create();
-    this.levelsButton.create();
+  }
+
+  update(){
+
   }
 }
